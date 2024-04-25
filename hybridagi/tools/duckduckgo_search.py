d91f1bef843e83cc7d91397e4fd084e374683829
YoanSallami <sallami.yoan@gmail.com>
2024-04-25 19:00:32 +0200

    Reduce default k for web search

--- hybridagi/tools/duckduckgo_search.py
+++ hybridagi/tools/duckduckgo_search.py
@@ -9,16 +9,16 @@ from ..parsers.prediction import PredictionOutputParser
 
 class DuckDuckGoSearchSignature(dspy.Signature):
     """You will be given an objective, purpose and context
-    Using the prompt to help you, you will infer the correct Google query"""
+    Using the prompt to help you, you will infer the correct DuckDuckGo query"""
     objective = dspy.InputField(desc = "The long-term objective (what you are doing)")
     context = dspy.InputField(desc = "The previous actions (what you have done)")
     purpose = dspy.InputField(desc = "The purpose of the action (what you have to do now)")
     prompt = dspy.InputField(desc = "The action specific instructions (How to do it)")
-    query = dspy.OutputField(desc = "The Google search query (only few words)")
+    query = dspy.OutputField(desc = "The DuckDuckGo search query (only few words)")
 
 class DuckDuckGoSearchTool(BaseTool):
 
-    def __init__(self, k: int = 3):
+    def __init__(self, k: int = 2):
         super().__init__(name = "DuckDuckGoSearch")
         self.predict = dspy.Predict(DuckDuckGoSearchSignature)
         self.k = k
